7d9aca13550d6251d0cdc255c487645e6adb93c4
Jon Chen <bsd@voltaire.sh>
2020-12-18 16:47:20 +0000

    set defaults

--- config.go
+++ config.go
@@ -34,47 +34,63 @@ var (
 	BuffetLevelType      LevelType = "buffet"
 )
 
+func Bool(b bool) *bool {
+	return &b
+}
+
+func Int(i int) *int {
+	return &i
+}
+
+func Int64(i int64) *int64 {
+	return &i
+}
+
+func String(s string) *string {
+	return &s
+}
+
 type ServerProperties struct {
 	// spawn protection radius
-	SpawnProtection             int
-	QueryPort                   int
-	GeneratorSettings           string
-	EnforceWhitelist            bool
-	ForceGamemode               bool
-	Gamemode                    Gamemode
-	EnableQuery                 bool
-	PlayerIdleTimeout           int64
-	Difficulty                  Difficulty
-	SpawnMonsters               bool
-	Pvp                         bool
-	LevelType                   LevelType
-	EnableStatus                bool
-	Hardcore                    bool
-	EnableCommandBlock          bool
-	NetworkCompressionThreshold int64
-	MaxPlayers                  int64
-	MaxWorldSize                int64
-	ResourcePackSha1            string
-	RconPort                    int
-	ServerPort                  int
-	Debug                       bool
-	ServerIp                    string
-	SpawnNpcs                   bool
-	AllowFlight                 bool
-	LevelName                   string
-	ViewDistance                int64
-	ResourcePack                string
-	SpawnAnimals                bool
-	WhiteList                   bool
-	RconPassword                string
-	GenerateStructures          bool
-	OnlineMode                  bool
-	MaxBuildHeight              int64
-	LevelSeed                   string
-	PreventProxyConnections     bool
-	Motd                        string
-	RateLimit                   int64
-	EnableRcon                  bool
+	SpawnProtection             *int
+	QueryPort                   *int
+	GeneratorSettings           *string
+	EnforceWhitelist            *bool
+	ForceGamemode               *bool
+	Gamemode                    *Gamemode
+	EnableQuery                 *bool
+	PlayerIdleTimeout           *int64
+	Difficulty                  *Difficulty
+	SpawnMonsters               *bool
+	Pvp                         *bool
+	LevelType                   *LevelType
+	EnableStatus                *bool
+	Hardcore                    *bool
+	EnableCommandBlock          *bool
+	NetworkCompressionThreshold *int64
+	MaxPlayers                  *int64
+	MaxWorldSize                *int64
+	ResourcePackSha1            *string
+	RconPort                    *int
+	ServerPort                  *int
+	Debug                       *bool
+	ServerIp                    *string
+	SpawnNpcs                   *bool
+	AllowFlight                 *bool
+	LevelName                   *string
+	ViewDistance                *int64
+	ResourcePack                *string
+	SpawnAnimals                *bool
+	WhiteList                   *bool
+	RconPassword                *string
+	GenerateStructures          *bool
+	OnlineMode                  *bool
+	MaxBuildHeight              *int64
+	LevelSeed                   *string
+	PreventProxyConnections     *bool
+	Motd                        *string
+	RateLimit                   *int64
+	EnableRcon                  *bool
 }
 
 func (cfg *ServerProperties) WriteTemplate(w io.Writer) error {
--- config_test.go
+++ config_test.go
@@ -9,59 +9,28 @@ import (
 
 type TemplateTestSuite struct {
 	suite.Suite
-
-	output bytes.Buffer
 }
 
 func (suite *TemplateTestSuite) TestRunScript() {
+	var output bytes.Buffer
 	cfg := &RunScript{
 		MemoryLimit: 512,
 	}
-	suite.Assert().NoError(cfg.WriteTemplate(&suite.output))
+	suite.Assert().NoError(cfg.WriteTemplate(&output))
 	expected := `#!/bin/sh
 
 java -Xms512M -Xmx512M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar /spigot.jar nogui 2>&1`
-	suite.Assert().Equal(expected, suite.output.String())
+	suite.Assert().Equal(expected, output.String())
 }
 
 func (suite *TemplateTestSuite) TestServerProperties() {
+	var output bytes.Buffer
 	cfg := &ServerProperties{
-		SpawnProtection:             16,
-		QueryPort:                   25565,
-		ForceGamemode:               false,
-		EnforceWhitelist:            false,
-		Gamemode:                    SurvivalGamemode,
-		EnableQuery:                 false,
-		PlayerIdleTimeout:           0,
-		Difficulty:                  EasyDifficulty,
-		SpawnMonsters:               true,
-		Pvp:                         true,
-		LevelType:                   DefaultLevelType,
-		EnableStatus:                true,
-		Hardcore:                    false,
-		EnableCommandBlock:          false,
-		NetworkCompressionThreshold: 256,
-		MaxPlayers:                  20,
-		MaxWorldSize:                29999984,
-		RconPort:                    25575,
-		ServerPort:                  25565,
-		Debug:                       false,
-		SpawnNpcs:                   true,
-		AllowFlight:                 false,
-		LevelName:                   "world",
-		ViewDistance:                10,
-		SpawnAnimals:                true,
-		WhiteList:                   false,
-		RconPassword:                "password",
-		GenerateStructures:          true,
-		OnlineMode:                  true,
-		MaxBuildHeight:              256,
-		PreventProxyConnections:     false,
-		Motd:                        "A Minecraft Server",
-		RateLimit:                   0,
-		EnableRcon:                  true,
+		RconPassword: String("password"),
+		Motd:         String("test"),
+		EnableRcon:   Bool(true),
 	}
-	suite.Assert().NoError(cfg.WriteTemplate(&suite.output))
+	suite.Assert().NoError(cfg.WriteTemplate(&output))
 	expected := `spawn-protection=16
 max-tick-time=60000
 query.port=25565
@@ -110,11 +79,11 @@ level-seed=
 prevent-proxy-connections=false
 use-native-transport=true
 enable-jmx-monitoring=false
-motd=A Minecraft Server
+motd=test
 rate-limit=0
 enable-rcon=true
 `
-	suite.Assert().Equal(expected, suite.output.String())
+	suite.Assert().Equal(expected, output.String())
 }
 
 func TestTemplate(t *testing.T) {
